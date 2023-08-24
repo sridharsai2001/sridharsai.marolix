ee6777e0296dec830b3982ae41afd87e80700b7d
sridharsai2001 <141006348+sridharsai2001@users.noreply.github.com>
2023-08-24 16:42:17 +0530

    Add files via upload

---
+++ two dictionary by adding values for common keys.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+d1 = {'a': 100, 'b': 200, 'c': 300}
+d2 = {'a': 300, 'b': 200, 'd': 400}
+
+combined_dict = Counter(d1)
+combined_dict.update(d2)
+
+print(combined_dict)
