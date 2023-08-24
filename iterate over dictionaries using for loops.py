045c17902c6040cfc2bc0d25050d1edae18bfe44
sridharsai2001 <141006348+sridharsai2001@users.noreply.github.com>
2023-08-24 16:39:15 +0530

    Add files via upload

---
+++ iterate over dictionaries using for loops.py
@@ -0,0 +1,14 @@
+
+my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+print("Iterating over keys:")
+for key in my_dict:
+    print(key)
+
+print("\nIterating over values:")
+for value in my_dict.values():
+    print(value)
+
+print("\nIterating over key-value pairs:")
+for key, value in my_dict.items():
+    print(f"Key: {key}, Value: {value}")
