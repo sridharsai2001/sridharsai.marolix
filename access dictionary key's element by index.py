2ef4f470a82bee8765ab161c2b1a6f1357f33290
sridharsai2001 <141006348+sridharsai2001@users.noreply.github.com>
2023-08-24 16:38:03 +0530

    Add files via upload

---
+++ access dictionary key's element by index.py
@@ -0,0 +1,11 @@
+data = {
+    "subject1": "physics",
+    "subject2": "math",
+    "subject3": "chemistry"
+}
+
+values_list = list(data.values())
+
+print(values_list[0])  
+print(values_list[1])  
+print(values_list[2])  
