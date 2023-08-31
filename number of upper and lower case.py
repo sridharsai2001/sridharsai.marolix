2cd8ceb85a91be4ed045191fc74219fb7eabf85a
sridharsai2001 <141006348+sridharsai2001@users.noreply.github.com>
2023-08-31 16:03:53 +0530

    Add files via upload

---
+++ number of upper and lower case.py
@@ -0,0 +1,16 @@
+def count_upper_lower(string):
+    upper_count = 0
+    lower_count = 0
+    
+    for char in string:
+        if char.isupper():
+            upper_count += 1
+        elif char.islower():
+            lower_count += 1
+    
+    return upper_count, lower_count
+
+sample_string = 'The quick Brow Fox'
+upper_count, lower_count = count_upper_lower(sample_string)
+print(f"Number of uppercase letters: {upper_count}")
+print(f"Number of lowercase letters: {lower_count}")
