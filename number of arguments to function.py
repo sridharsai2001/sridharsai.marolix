9b38079eafe5115638b0ade9bb434a5b35ff59c2
sridharsai2001 <141006348+sridharsai2001@users.noreply.github.com>
2023-08-31 16:03:30 +0530

    Add files via upload

---
+++ number of arguments to function.py
@@ -0,0 +1,10 @@
+def sum_of_values(*args):
+    total_sum = sum(args)
+    return total_sum
+
+# Example usage
+result = sum_of_values(1, 2, 3, 4, 5)
+print(result)  
+
+result = sum_of_values(10, 20, 30)
+print(result)  
